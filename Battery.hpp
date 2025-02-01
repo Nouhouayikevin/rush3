59c499a771a45f5b4b8f3191e1011fad237e3760
ADJINDA Fania ifede <fania.adjinda@epitech.eu>
2025-02-01 20:34:27 +0100

    do task of module battery.cpp

---
+++ Battery.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2025
+** Battery.hpp
+** File description:
+** file
+*/
+
+#ifndef   ICPU_H
+    #define ICPU_H
+#include "IModule.hpp"
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#define Byte 1024
+
+class Battery :public IModule {
+    private:
+    int capacity;
+    std :: string status;
+void init();
+void update();
+std::string getDisplayString() const;
+std::string getGraphicString() const;
+};
+#endif
